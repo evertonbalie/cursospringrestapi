c74647806668f54c62d46cecca71615ee285f4cf
evertonbalie <evertonbalie@gmail.com>
2025-05-23 17:07:26 -0300

    aaaaa

--- src/main/java/curso/api/rest/model/Veiculos.java
+++ src/main/java/curso/api/rest/model/Veiculos.java
@@ -21,7 +21,7 @@ public class Veiculos implements Serializable{
 	private String ano;
 	
 	
-	//testeCom main1234delete  veiculo
+	//testeCom main1234delete  veiculo agora vai
 	public Integer getId() {
 		return id;
 	}
